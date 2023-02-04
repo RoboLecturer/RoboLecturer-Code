c68205d2bf2948655c9900385721c05716da9585
Keith <chengchunhong620@gmail.com>
2023-02-04 03:17:21 +0000

    add ros wrapper

---
+++ nlp_ros/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/dovakeith/HCR/nlp_ros/src
+# Build directory: /home/dovakeith/HCR/nlp_ros/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("nlp_pkg")
---
+++ nlp_ros/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/dovakeith/HCR/nlp_ros/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/dovakeith/HCR/nlp_ros/devel;/opt/ros/noetic'.split(';') if '/home/dovakeith/HCR/nlp_ros/devel;/opt/ros/noetic' != '' else []
