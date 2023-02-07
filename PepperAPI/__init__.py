39b0db6f3b0e8a6513ebc5c992d4afe849014024
rachung2510 <rachung2510@gmail.com>
2023-02-07 19:15:01 +0000

    initiate node with incrementing xmlrpc & tcpros ports

--- PepperAPI/__init__.py
+++ PepperAPI/__init__.py
@@ -1,4 +1,21 @@
 import rospy
+import rosnode
 
 def init(node):
-	rospy.init_node(node, anonymous=True)
+	# XMLRPC & TCPROS ports to start from 45100 & 45101
+	_xmlrpc, _tcpros = 45100, 45101
+
+	# Since every script calling the API requires a separate node,
+	# check for nodes with the same name and append an incrementing counter
+	# to different nodes from different scripts
+	nodes = rosnode.get_node_names()
+	num = 0
+	node_name = "%s_%d" % (node, num)
+	while "/"+node_name in nodes:
+		num += 1
+		_xmlrpc += 2
+		_tcpros += 2
+		node_name = "%s_%d" % (node, num)
+
+	# Initialize the node
+	rospy.init_node(node_name, xmlrpc_port=_xmlrpc, tcpros_port=_tcpros)
