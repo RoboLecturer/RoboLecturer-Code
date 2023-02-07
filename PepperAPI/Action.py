d5b7cca298240c84b2c5fd8ab524ddf1b9387ebc
rachung2510 <rachung2510@gmail.com>
2023-02-07 19:12:33 +0000

    added tts proxy for ALTextToSpeech

--- PepperAPI/Action.py
+++ PepperAPI/Action.py
@@ -1,13 +1,23 @@
 from .Publisher import *
+from naoqi import ALProxy
 
-def Request(api, value):
+
+def Request(api_name, api_params):
 	
+	# Global parameters
+	ROBOT_IP = "192.168.0.102"
+	ROBOT_PORT = 9559
+
+	# API callbacks
 	"""
 	Send message to be said by Pepper
-	value: [msg]
+	@param api_params: {
+		"value": message to be sent
+	}
 	"""
-	if api == "ALTextToSpeech":
-		msg = value[0]
-		tts_publisher.publish(msg)
+	if api_name == "ALTextToSpeech":
+		msg = api_params["value"]
+		tts = ALProxy("ALTextToSpeech", ROBOT_IP, ROBOT_PORT)
+		tts.say(msg)
 
 	return
