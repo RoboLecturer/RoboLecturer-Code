e7ae0f213cc6d7a57b79fc4266ec1edb96b5458c
rachung2510 <rachung2510@gmail.com>
2023-02-11 20:51:29 +0000

    add APIs for MVP

--- api/scripts/PepperAPI/Subscriber.py
+++ api/scripts/PepperAPI/Subscriber.py
@@ -1,7 +1,7 @@
 import rospy, cv2
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
-from api.msg import Coords
+from api.msg import CVInfo
 
 # Parent subscriber class
 class Subscriber:	
@@ -19,7 +19,7 @@ class Subscriber:
 				self.subscriber.unregister()
 
 		self.subscriber = rospy.Subscriber(topic, payload_type, finalCallback)
-		self.log()
+		# self.log()
 		# my implementation of rospy.spin()
 		while self.spin and not rospy.is_shutdown():
 			rospy.rostime.wallsleep(0.5)
@@ -32,7 +32,7 @@ class Subscriber:
 # Child subscriber classes
 """receive simple string msg"""
 class StringSubscriber(Subscriber, object):
-	def __init__(self, topic, callback, listen_once=False):
+	def __init__(self, topic, callback, listen_once=True):
 		super(StringSubscriber, self).__init__(
 			"StringSubscriber", 
 			topic, 
@@ -41,12 +41,12 @@ class StringSubscriber(Subscriber, object):
 			listen_once)
 
 """receive array of xy float coordinates"""
-class CoordsSubscriber(Subscriber, object):
+class CVInfoSubscriber(Subscriber, object):
 	def __init__(self, topic, callback):
-		super(CoordsSubscriber, self).__init__(
-			"CoordsSubscriber", 
+		super(CVInfoSubscriber, self).__init__(
+			"CVInfoSubscriber", 
 			topic, 
-			Coords,
+			CVInfo,
 			callback, 
 			listen_once=True)
 
