151bfe2c07d062c66103f2236578c8d00ecb5e22
rachung2510 <rachung2510@gmail.com>
2023-02-11 20:52:03 +0000

    simulate info sending/receiving of CV module

--- api/scripts/pub.py
+++ api/scripts/pub.py
@@ -2,22 +2,45 @@
 
 import PepperAPI
 from PepperAPI import Action, Info
-import cv2
+import time, sys
+
+def main():
+	
+	time.sleep(2)
+	print("\nSending trigger_hand_detection...")
+	Info.Send("TriggerHandDetection")
+
+	print("\nReceiving hand info...")
+	hand1 = Info.Request("RaisedHandInfo")
+	print(hand1)
+
+	time.sleep(2)
+	print("\nSending trigger_attentiveness_detection...")
+	Info.Send("TriggerAttentivenessDetection")
+
+	print("\nWaiting for trigger_noise_detection...")
+	Info.Request("TriggerNoiseDetection")
+
 
 if __name__ == "__main__":
-	# Initialise API module
+	## Initialise API module
 	PepperAPI.init("api_module")
 
-	# Publish info
-	# Info.Send("RLSimpleMsg", {"value": "Hello World"})
-	# Info.Send("RLHandLocation", {"value":[1.23, 5.42]})
-	
-	# Send image
+	counter = 0
+	try:
+		while True:
+			print("\nTrial %d" % counter)
+			main()
+			counter += 1
+	except:
+		sys.exit(0)
+
+	## Send image
 	# img = cv2.imread("image.jpg")
 	# img_blur = cv2.GaussianBlur(img,(3,3), sigmaX=0, sigmaY=0)
 	# sobel = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=1, ksize=5)
-	# Info.Send("RLImage", {"value": sobel})
+	# Info.Send("Image", {"value": sobel})
 
-	# Request to play audio	
-	Action.Request("ALTextToSpeech", {"value":"Hello World"})
+	## Action requests
+	# Action.Request("ALTextToSpeech", {"value":"Hello World"})
 	# Action.Request("ALAudioPlayer", {"path":"/home/user/sample.mp3"})
--- api/scripts/sub.py
+++ api/scripts/sub.py
@@ -2,16 +2,45 @@
 
 import PepperAPI
 from PepperAPI import Info
-import cv2
+import time, sys
+
+def simulate_cv_module():
+	# Waits for trigger_hand_detection, sends hand_info,
+	# then waits for trigger_attentiveness_detection, then sends
+	# trigger_noise_detection to Speech
+	
+	print("\nWaiting for trigger_hand_detection...")
+	if not Info.Request("TriggerHandDetection"):
+		return
+
+	time.sleep(2)
+	print("\nSending hand info...")
+	Info.Send("RaisedHandInfo", {
+        "bounding_box": (1.0, 2.0, 3.0, 4.0),
+        "frame_res": (1920, 1080),
+        "confidence_score": 0.96
+    })
+
+	print("\nWaiting for trigger_attentiveness_detection...")
+	if not Info.Request("TriggerAttentivenessDetection"):
+		return
+
+	time.sleep(2)
+	print("\nSend trigger_noise_detection...")
+	Info.Send("TriggerNoiseDetection")
+
+	return
 
 if __name__ == "__main__":
-	# Initialise API module
+	## Initialise API module
 	PepperAPI.init("api_module")
 
-	# Subscribe
-	msg = Info.Request("RLSimpleMsg", {"listen_once":True})
-	print("Received message: %s" % str(msg))
-	# msg = Info.Request("RLHandLocation", {})
+	counter = 0
+	try:
+		while True:
+			print("\nTrial %d" % counter)
+			simulate_cv_module()
+			counter += 1
+	except:
+		sys.exit(0)
 
-	# img = Info.Request("RLImage", {})
-	# cv2.imwrite("edges.jpg", img)
