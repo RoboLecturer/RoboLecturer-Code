1360d9df73457957a76b840e225fdc68bb762df5
rachung2510 <rachung2510@gmail.com>
2023-02-13 12:23:27 +0000

    initiate kinematics subscribers in multiple threads

--- api/scripts/PepperAPI/Action.py
+++ api/scripts/PepperAPI/Action.py
@@ -1,10 +1,13 @@
+import time
+import threading
+import rospy
 from .Publisher import *
 from .Subscriber import *
 from PepperAPI import *
 import paramiko
 
 def Request(api_name, api_params):
-	
+
 	# API callbacks
 	if api_name == "ALTextToSpeech":
 		"""Send message to be said by Pepper
@@ -14,6 +17,7 @@ def Request(api_name, api_params):
 		"""
 		msg = api_params["value"]
 		tts_publisher.publish(msg)
+		return True
 	
 	if api_name == "ALAudioPlayer":
 		"""Request for audio file to be played through Pepper's speakers
@@ -38,6 +42,7 @@ def Request(api_name, api_params):
 
 		# Publish msg to kinematics module to play audio
 		audio_player_publisher.publish(filename)
+		return True
 
 	return
 
@@ -49,19 +54,23 @@ def Listen():
 	# Import NAOqi modules
 	from naoqi import ALProxy
 
+	# Callback for ALTextToSpeech
 	def tts_callback(msg):
+		rospy.loginfo("Pepper say: %s" % msg.data)
 		tts = ALProxy("ALTextToSpeech", ROBOT_IP, ROBOT_PORT)
-		tts.say(msg)
+		tts.say(msg.data)
 		return True
 
+	# Callback for ALAudioPlayer
 	def audio_player_callback(msg):
 		filename = msg.data
-		rospy.loginfo("Play audio: %s" % filename)
+		rospy.loginfo("Pepper play audio: %s" % filename)
 		ap = ALProxy("ALAudioPlayer", ROBOT_IP, ROBOT_PORT)
 		audio_file = PEPPER_AUDIO_PATH + filename
 		ap.post.playFile(audio_file)
 		return True
 
+	# Callback for pointing at raised hand
 	def point_callback(msg):
 		# parse msg
 		x,y,w,h = msg.x, msg.y, msg.w, msg.h
@@ -102,9 +111,8 @@ def Listen():
 		point_z = Z_UP - Z_DOWN * center_y / frame_height
 
 		# actuate
-		rospy.loginfo("Point %s at x=%.2f y=%.2f, z=%.2f" % 
+		rospy.loginfo("Pepper point %s at x=%.2f y=%.2f, z=%.2f" % 
 			(effector, point_x, point_y, point_z))
-		return
 
 		tracker = ALProxy("ALTracker", ROBOT_IP, ROBOT_PORT)
 		posture = ALProxy("ALRobotPosture", ROBOT_IP, ROBOT_PORT)
@@ -117,11 +125,44 @@ def Listen():
 
 		return True
 
+
+	# Define event to terminate thread on command
+	event = threading.Event()
+	event.set()
+
+	# Initiate subscriber without listening
+	# spin implemented in thread so it can be terminated on command
+	def subscribe_listen(init_subscriber):
+		init_subscriber()
+		while event.is_set() and not rospy.is_shutdown():
+			rospy.rostime.wallsleep(0.5)
+
 	# General TTS to be used by any module
-	# StringSubscriber("tts", tts_callback, listen_once=False)
+	thread_tts = threading.Thread(target=subscribe_listen, args=(
+		lambda: StringSubscriber("tts", tts_callback, listen=False),
+		))
 
 	# ALAudioPlayer used by speech module
-	# StringSubscriber("audio_player", audio_player_callback, listen_once=False)
-	
+	thread_ap = threading.Thread(target=subscribe_listen, args=(
+		lambda: StringSubscriber("audio_player", audio_player_callback, listen=False),
+		))
+
 	# Hands info sent by CV module
-	CVInfoSubscriber("raised_hand", point_callback)
+	thread_hand = threading.Thread(target=subscribe_listen, args=(
+		lambda: CVInfoSubscriber("raised_hand", point_callback, listen=False),
+		))
+
+	# Run threads
+	thread_tts.start()
+	thread_ap.start()
+	thread_hand.start()
+
+	# Exit when KeyboardInterrupt
+	try:
+		while True:
+			time.sleep(1)
+	except KeyboardInterrupt:
+		event.clear()
+		thread_tts.join()
+		thread_ap.join()
+		thread_hand.join()
