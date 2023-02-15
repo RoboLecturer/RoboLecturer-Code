713e6287c350de2d34af1ec619fac4cb84004b40
Keith <chengchunhong620@gmail.com>
2023-02-15 17:12:16 +0000

    add info api

--- nlp_ros/src/nlp_pkg/src/PepperAPI/Publisher.py
+++ nlp_ros/src/nlp_pkg/src/PepperAPI/Publisher.py
@@ -1,29 +1,125 @@
 import rospy
 from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from api.msg import CVInfo, State
+from cv_bridge import CvBridge
 
-"""
-Parent publisher class
-"""
-class Publisher:	
-	def __init__(self, topic, payload_type):
-		self.publisher = rospy.Publisher(topic, payload_type, queue_size=10)
+# Parent publisher class
+class Publisher:
+	def __init__(self, name, topic, payload_type):
+		self.name = name
+		self.topic = topic
+		self.publisher = rospy.Publisher(self.topic, payload_type, queue_size=10)
+		# self.log()
 
-"""
-Child publisher classes
-"""
-class TTS_Publisher(Publisher, object):
+	# Log info about publisher
+	def log(self):
+		print("[%s] topic=%s" % (self.name, self.topic))
+
+# Child publisher classes
+"""publish string msg"""
+class StringPublisher(Publisher, object):
 	def __init__(self, topic):
-		super(TTS_Publisher, self).__init__(topic, String)
+		super(StringPublisher, self).__init__(
+			"StringPublisher",
+			topic, 
+			String)
 
-	def publish(self, message):
-		self.publisher.publish(message)
-		rospy.loginfo("Publish: %s" % message)
+	def publish(self, msg):
+		msg = str(msg)
+		while self.publisher.get_num_connections() == 0:
+			pass			
+		self.publisher.publish(msg)
+		rospy.loginfo("[%s] %s: %s" % (self.name, self.topic, msg))
+
+"""publish array of xy float coordinates"""
+class CVInfoPublisher(Publisher, object):
+	def __init__(self, topic):
+		super(CVInfoPublisher, self).__init__(
+			"CVInfoPublisher",
+			topic,
+			CVInfo)
+
+	def publish(self, bounding_box, frame_res, confidence_score):
+		msg = CVInfo()
+		msg.x, msg.y, msg.w, msg.h = bounding_box
+		msg.frame_width = frame_res[0]
+		msg.frame_height = frame_res[1]
+		msg.score = confidence_score
+		while self.publisher.get_num_connections() == 0:
+			pass			
+		self.publisher.publish(msg)
+		rospy.loginfo("[%s] %s: Published hand info" % (self.name, self.topic))
+
+"""publish image"""
+class ImagePublisher(Publisher, object):
+	def __init__(self, topic):
+		super(ImagePublisher, self).__init__(
+			"ImagePublisher",
+			topic,
+			Image)
+
+	def publish(self, image):
+		br = CvBridge()
+		while self.publisher.get_num_connections() == 0:
+			pass			
+		self.publisher.publish(br.cv2_to_imgmsg(image))
+		rospy.loginfo("[%s] %s: Published image" % (self.name, self.topic))
+
+"""publish state info"""
+class StatePublisher(Publisher, object):
+	def __init__(self, topic):
+		super(StatePublisher, self).__init__(
+			"StatePublisher",
+			topic,
+			State)
+
+	def publish(self, state_dict):
+		msg = State()
+		msg.Start = state_dict["Start"]
+		msg.AnyQuestions = state_dict["AnyQuestions"]
+		msg.NoiseLevel = state_dict["NoiseLevel"]
+		msg.Attentiveness = state_dict["Attentiveness"]
+		msg.NoQuestionsLoop = state_dict["NoQuestionsLoop"]
+		while self.publisher.get_num_connections() == 0:
+			pass			
+		self.publisher.publish(msg)
 
 
-"""
-Initiate publishers
-"""
-tts_publisher = TTS_Publisher("tts_topic")
+# ==============================================================
+# Initiate publishers
+simple_msg_publisher = StringPublisher("simple_msg")
+image_publisher = ImagePublisher("image")
+tts_publisher = StringPublisher("tts")
 
-# def init(node):
-# 	rospy.init_node(node, anonymous=True)
+## WEB
+slides_publisher = StringPublisher("slides")
+take_control_publisher = StringPublisher("take_control")
+
+## CV
+hand_publisher = CVInfoPublisher("raised_hand")
+face_publisher = CVInfoPublisher("faces")
+trigger_noise_detection_publisher = StringPublisher("trigger_noise_detection")
+increment_loop_counter_publisher = StringPublisher("increment_loop_counter")
+
+## NLP
+lecture_script_publisher = StringPublisher("lecture_script")
+answer_publisher = StringPublisher("answer")
+joke_publisher = StringPublisher("joke")
+
+## SPEECH
+question_publisher = StringPublisher("question")
+audio_player_publisher = StringPublisher("audio_player_topic")
+trigger_attentiveness_detection_publisher = StringPublisher("trigger_attentiveness_detection")
+
+## KINEMATICS
+trigger_hand_detection_publisher = StringPublisher("trigger_hand_detection")
+trigger_listen_publisher = StringPublisher("trigger_listen")
+
+## CONTROL
+change_slide_publisher = StringPublisher("change_slide")
+state_publisher = StatePublisher("state")
+
+## SHARED
+trigger_quiz_publisher = StringPublisher("trigger_quiz")
+trigger_joke_publisher = StringPublisher("trigger_joke")
--- nlp_ros/src/nlp_pkg/src/PepperAPI/Subscriber.py
+++ nlp_ros/src/nlp_pkg/src/PepperAPI/Subscriber.py
@@ -1,34 +1,71 @@
-import rospy
+import rospy, cv2
 from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from api.msg import CVInfo, State
 
-"""
-Parent subscriber class
-"""
+# Parent subscriber class
 class Subscriber:	
-	def __init__(self, topic, payload_type, callback, listen_once=False):
+	def __init__(self, name, topic, payload_type, callback, listen):
+		self.name = name
+		self.topic = topic
 		self.subscriber = None
-		self.spin = True
+		self.spin = listen
 
 		def finalCallback(data):
 			callback(data)
-			if listen_once:
+			if listen:
 				self.spin = False
 				self.subscriber.unregister()
 
 		self.subscriber = rospy.Subscriber(topic, payload_type, finalCallback)
-		# implementation of rospy.spin()
-		while self.spin:
+		self.log()
+
+		# my implementation of rospy.spin()
+		while self.spin and not rospy.is_shutdown():
 			rospy.rostime.wallsleep(0.5)
 
-"""
-Child subscriber classes
-"""
-class TTS_Subscriber(Subscriber, object):
-	def __init__(self, topic, callback, listen_once=False):
-		super(TTS_Subscriber, self).__init__(topic, String, callback, listen_once)
+	# Log information about subscriber
+	def log(self):
+		print("[%s] topic=%s, listen=%s" % (self.name, self.topic, self.spin))
 
-"""
-Initiate subscribers
-"""
-# def init(node):
-# 	rospy.init_node(node, anonymous=True)
+
+# Child subscriber classes
+"""receive string"""
+class StringSubscriber(Subscriber, object):
+	def __init__(self, topic, callback, listen=True):
+		super(StringSubscriber, self).__init__(
+			"StringSubscriber", 
+			topic, 
+			String, 
+			callback, 
+			listen)
+
+"""receive feature info about raised hand or detected face"""
+class CVInfoSubscriber(Subscriber, object):
+	def __init__(self, topic, callback, listen=True):
+		super(CVInfoSubscriber, self).__init__(
+			"CVInfoSubscriber", 
+			topic, 
+			CVInfo,
+			callback, 
+			listen)
+
+"""receive image"""
+class ImageSubscriber(Subscriber, object):
+	def __init__(self, topic, callback, listen=True):
+		super(ImageSubscriber, self).__init__(
+			"ImageSubscriber", 
+			topic, 
+			Image,
+			callback, 
+			listen)
+		
+"""receive state"""
+class StateSubscriber(Subscriber, object):
+	def __init__(self, topic, callback, listen=True):
+		super(StateSubscriber, self).__init__(
+			"StateSubscriber", 
+			topic, 
+			State,
+			callback, 
+			listen)
