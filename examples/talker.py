f1b0635922137f7739415e1fc0a938aab397dd56
rachung2510 <rachung2510@gmail.com>
2023-02-21 11:22:34 +0000

    remove examples

--- examples/talker.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import PepperAPI
-from PepperAPI import Info, Publisher
-
-if __name__ == '__main__':
-	PepperAPI.init("subteam_node")
-	Info.Send("RLTest", {"value":"Hello World"})
-	Info.Request("RLTest", {"listen_once":True})
