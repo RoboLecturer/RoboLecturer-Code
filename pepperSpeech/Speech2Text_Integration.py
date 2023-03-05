e3b5af26c8a1b6f5848e2102ff235b25ba5a4d2d
Antonis Papaoikonomou <36318735+antonypap@users.noreply.github.com>
2023-03-05 18:14:57 +0000

    Modified US voice, S2T functions

--- pepperSpeech/Speech2Text_Integration.py
+++ pepperSpeech/Speech2Text_Integration.py
@@ -3,40 +3,10 @@
 import speech_recognition as sr
 import PepperAPI
 from PepperAPI import info
+from speech2text import *
 
 
 filename = "6241-61943-0027.flac"
-
-def Sphinx(audio):
-    # recognize speech using Sphinx
-    try:
-        return "Sphinx thinks you said " + r.recognize_sphinx(audio)
-    except sr.UnknownValueError:
-        print("Sphinx could not understand audio")
-    except sr.RequestError as e:
-        print("Sphinx error; {0}".format(e))
-
-def GSR(audio): 
-    # recognize speech using Google Speech Recognition
-    try:
-        # for testing purposes, we're just using the default API key
-        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-        # instead of `r.recognize_google(audio)`
-        print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
-    except sr.UnknownValueError:
-        print("Google Speech Recognition could not understand audio")
-    except sr.RequestError as e:
-        print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
-def whisper(audio): 
-# recognize speech using whisper
-    try:
-        print("Whisper thinks you said " + r.recognize_whisper(audio, language="english"))
-    except sr.UnknownValueError:
-        print("Whisper could not understand audio")
-    except sr.RequestError as e:
-        print("Could not request results from Whisper")
-r = sr.Recognizer()
 
 if __name__ == "__main__":
     #initialize API
@@ -48,6 +18,7 @@ if __name__ == "__main__":
     #print(sr.Microphone.list_microphone_names())
 
     # Test with mic
+    r = sr.Recognizer()
     with sr.Microphone(device_index=0) as source:
         print("Say something!")
         mic_input = r.listen(source)
