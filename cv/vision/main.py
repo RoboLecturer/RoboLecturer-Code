48b59322a86c0a48bf03ece7755a3e0e868c9f49
MattG-bci <mateusz.f.grzybowski@gmail.com>
2023-03-06 14:39:51 +0000

    Deleting the PepperAPI from the utils directory

--- cv/vision/main.py
+++ cv/vision/main.py
@@ -34,11 +34,9 @@ def face_engagement_detection(face_model, landmark_predictor, face_2d, face_3d, 
     # get faces
     faces = face_model.detectMultiScale(frame, scaleFactor=1.2, minNeighbors=5, minSize=(64, 64))
     # display boxes around faces
-    # detect/display boxes around hands
     for (x_face, y_face, w_face, h_face) in faces:
         cv2.rectangle(frame, (x_face, y_face), (x_face+w_face, y_face+h_face), (255, 0, 0), 2)
         roi_color_face = frame[y_face:y_face+h_face, x_face:x_face+w_face]
-       # cv2.putText(frame, f"Location: ({x_face}, {y_face})", org=(x_face, y_face-5), fontFace=cv2.FONT_HERSHEY_SIMPLEX , fontScale=1, color=(0, 0, 255), thickness=2, lineType=2)
         
         # Computing landmarks of a detected face and computing an engagement score
         landmark_results = landmark_predictor.process(frame)
@@ -46,20 +44,6 @@ def face_engagement_detection(face_model, landmark_predictor, face_2d, face_3d, 
         engagement = compute_engagement_score(x, y, z)
         engagement_record.append(engagement)
         cv2.putText(frame, f"Engagement:  {engagement:.3f}", org=(x_face, y_face-5), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(0, 0, 255), thickness=2, lineType=2)
-
-    # Hand detection here, independent from the face detection.
-   # t1 = threading.Thread(target=closed_hand_model, args=(closed_hand_model, frame, closed_hands))
-   # t2 = threading.Thread(target=open_hand_model, args=(open_hand_model, frame, open_hands))
-
-   # t1.start()
-   # t2.start()
-   # t1.join()
-   # t2.join()
-   # coords = hand_detector_mp(mp_model, frame, closed_hands)
-   # closed_hands.append(coords)
-    
-   # if len(closed_hands) > 0:
-   #     print(f"Hand Detected: {closed_hands[-1]}")
     
     return frame, engagement_record
 
@@ -93,12 +77,8 @@ def hand_detector_mp(model, frame):
     return None
 
 
-def format_data(raw_data):
-    return
-
 def send_to_pepper(hand_data):
-    unique_hands = list(set(hand_data))
-    
+    unique_hands = list(set(hand_data))    
     for data in unique_hands:
         dic_data = {"bounding_box": (data[-1][0], data[-1][1], 100, 100),
                 "frame_res": (1920, 1080),
@@ -107,28 +87,16 @@ def send_to_pepper(hand_data):
         Info.Send("RaisedHandInfo", dic_data)
     return
 
-def main():
-    # init of CV module
-   # PepperAPI.init("cv_node")
-    print("API Initialised...")
-    # set up camera
-    camera = set_up_camera()
-    
-    # set up pre-trained models
-    face_detect_model       = cv2.CascadeClassifier("./utils/models/face_detection.xml") # loading the pre-trained face detection model from OpenCV.
-   # closed_hand_raise_model = cv2.CascadeClassifier("./utils/models/closed_hand.xml")
-   # open_hand_raise_model   = cv2.CascadeClassifier("./utils/models/open_hand.xml")
-    landmark_predictor = landmark_model()
-    mp_hand_model = mp.solutions.hands.Hands()
+
+def main(camera, face_detect_model, mp_hand_model, landmark_predictor):
+    hands = []
     # main loop
     while True:
         frame  = get_camera_input(camera)
         face_3d = []
         face_2d = []
-        hands = []
 
         Info.Request("State", {"name": "Start"})
-        
         Info.Request("TriggerHandDetection")
 
         coordinates = hand_detector_mp(mp_hand_model, frame)
@@ -139,11 +107,8 @@ def main():
             Info.Send("State", {"AnyQuestions": "NoHandsRaised"})
 
         else:
-
             Info.Send("State", {"AnyQuestions": "HandsRaised"})
-
             Info.Send("NumHands", {"value": num_hands})
-
             send_to_pepper(hands)
 
             while Info.Request("State", {"name": "AnyQuestions", "print": False}) != "NoHandsRaised":
@@ -155,7 +120,6 @@ def main():
             return
 
         # Start checking the engagement/attentiveness of the class
-
         frame2, engagement_list = face_engagement_detection(face_detect_model, landmark_predictor, face_2d, face_3d, frame)
         mean_engagement = np.mean(np.array(engagement_list))
 
@@ -166,23 +130,17 @@ def main():
         else:
             Info.Send("State": {"Attentiveness": "Attentive"})
 
-
         Info.Request("State", {"name": "NoQuestionsLoop"})
         return
 
 
-       # k = cv2.waitKey(30) 
-       # if k == 27: # press 'ESC' to quit
-       #     break
-       # cv2.imshow("video", frame2)
-        #cleaned_data = format_data(raw_data)
-        
-   # print("Sending...") 
-   # send_to_pepper(closed_hands)
-   # print("Sent")
-
-   # print("FINISHED")
-   # print(list(set(closed_hands)))
-   # print(list(set(open_hands)))
-
-main()
+if __name__ == "__main__":
+    # Initialising the connection.
+    PepperAPI.init("cv_node")
+    print("API Initialised")
+    # Setting up the camera and pretrained models.
+    camera = set_up_camera()
+    face_model = cv2.CascadeClassifier("./utils/models/face_detection.xml")
+    hand_model = mp.solutions.hands.Hands()
+    landmark_predictor = landmark_model()
+    main(camera, face_model, hand_model, landmark_predictor)
