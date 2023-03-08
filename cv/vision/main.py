5f16364c6bca683e2510fc616321c4493592071f
lukemkU <lucas.iij27@gmail.com>
2023-03-08 13:30:48 +0000

    Added organized YOLOface files

--- cv/vision/main.py
+++ cv/vision/main.py
@@ -162,3 +162,75 @@ if __name__ == "__main__":
     hand_model = mp.solutions.hands.Hands()
     landmark_predictor = landmark_model()
     main(camera, face_model, hand_model, landmark_predictor)
+
+
+
+
+### YOLO main
+
+""" 
+### Run --> python main.py --weights utils/models/YOLOface/weights/yolov7s-face.pt --save-txt --nosave
+### Print CPU/GPU device: utils/models/YOLOface/utils/torch_utils.py --> select_device() --> uncomment logger.info (line 85)
+### Print total number of faces + inference time per frame: utils/models/YOLOface/detect.py --> uncomment print command (line 162)
+
+import argparse
+import numpy as np
+import cv2
+
+from utils.models.YOLOface.detect import detect, config_net
+from utils.models.YOLOface.utils.general import check_requirements
+from utils.models.YOLOface.utils.general import check_requirements
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--weights', nargs='+', type=str, default='yolov5s.pt', help='model.pt path(s)')
+parser.add_argument('--source', type=str, default='utils/models/YOLOface/data/images', help='source')  # file/folder, 0 for webcam
+parser.add_argument('--img-size', nargs= '+', type=int, default=640, help='inference size (pixels)')
+parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
+parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
+parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+parser.add_argument('--view-img', action='store_true', help='display results')
+parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+parser.add_argument('--save-txt-tidl', action='store_true', help='save results to *.txt in tidl format')
+parser.add_argument('--save-bin', action='store_true', help='save base n/w outputs in raw bin format')
+parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+parser.add_argument('--save-crop', action='store_true', help='save cropped prediction boxes')
+parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
+parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
+parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+parser.add_argument('--augment', action='store_true', help='augmented inference')
+parser.add_argument('--update', action='store_true', help='update all models')
+parser.add_argument('--project', default='runs/detect', help='save results to project/name')
+parser.add_argument('--name', default='exp', help='save results to project/name')
+parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+parser.add_argument('--line-thickness', default=1, type=int, help='bounding box thickness (pixels)')
+parser.add_argument('--hide-labels', default=False, action='store_true', help='hide labels')
+parser.add_argument('--hide-conf', default=False, action='store_true', help='hide confidences')
+parser.add_argument('--kpt-label', type=int, default=5, help='number of keypoints')
+opt = parser.parse_args()
+print(opt)
+check_requirements(exclude=('tensorboard', 'pycocotools', 'thop'))
+
+model, imgsz, stride = config_net(opt=opt)
+
+# Defining frame sizes
+frame_size_x = 640
+frame_size_y = 640
+
+# Replace for the camera loop
+for i in range(10):
+    frame = cv2.imread('utils\models\YOLOface\data\images\classroom_01.jpg')
+
+    # Returns the list of all faces coordinates - normalized values
+    faces_coords = detect(opt, frame, model, imgsz, stride)
+    faces_coords = np.array(faces_coords)
+
+# Getting the coordinates in pixels
+faces_coords[:,0] *= frame_size_x     # Center x of each bounding box
+faces_coords[:,1] *= frame_size_y     # Center y of each bounding box
+faces_coords[:,2] *= frame_size_x     # Height of each bounding box
+faces_coords[:,3] *= frame_size_y     # Width  of each bounding box
+faces_coords = faces_coords.astype(int)
+
+#print(faces_coords)
+#print(10*faces_coords[:,1])
+ """
