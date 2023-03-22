bfd86a4ef3d24929e9a8d96f2d733c952846dafd
rachung2510 <rachung2510@gmail.com>
2023-03-22 13:08:36 +0000

    resize frame & add hotkey to save image

--- scripts/read_frame.py
+++ scripts/read_frame.py
@@ -5,18 +5,25 @@ import vision_definitions
 import Image
 
 vid = ALProxy("ALVideoDevice", "192.168.0.110", 9559)
-resolution = vision_definitions.kQQVGA
+resolution = vision_definitions.kQVGA
 colorSpace = vision_definitions.kRGBColorSpace
 fps = 10
 subscriberID = vid.subscribe("subscriberID", resolution, colorSpace, fps)
+num = 0
 while True:
 	img = vid.getImageRemote(subscriberID)
 	imgWidth, imgHeight = img[0], img[1]
 	array = img[6]
 	im = Image.frombytes("RGB", (imgWidth, imgHeight), array)
-	mat = np.array(im)[:,:,::-1] # convert to BGR
-	cv2.imshow("img", mat)
+	frame = np.array(im)[:,:,::-1] # convert to BGR
+	frame = cv2.resize(frame, (640,480), interpolation=cv2.INTER_AREA)
+	cv2.imshow("img", frame)
 	key = cv2.waitKey(1) & 0xFF
 	if key == ord('q'):
 		break
+	if key == ord('p'):
+		cv2.imwrite("grid%d.jpg" % num, frame)
+		num += 1
+		print("Image saved to grid%d.jpg." % num)
+
 cv2.destroyAllWindows()
