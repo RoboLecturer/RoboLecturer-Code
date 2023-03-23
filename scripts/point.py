4317333437b022488987c1dd877fa5350dcc7e34
rachung2510 <rachung2510@gmail.com>
2023-03-23 12:21:31 +0000

    merge working dir to remote

--- scripts/point.py
+++ scripts/point.py
@@ -3,6 +3,6 @@ import os
 tracker = ALProxy("ALTracker", os.environ["NAO_IP"], 9559)
 frame = 0 # Torso=0, World=1, Robot=2
 max_speed = 0.5
-tracker.pointAt("LArm", [1, 0.7, 0.3], frame, max_speed)
+tracker.pointAt("LArm", [1, 0.9, 0.3], frame, max_speed)
 
 
--- scripts/stand_init.py
+++
@@ -1,3 +0,0 @@
-from naoqi import ALProxy, ALBroker
-posture = ALProxy("ALRobotPosture", "192.168.0.110", 9559)
-posture.applyPosture("StandInit",0.2)
