0f22adc4626265d187f62f748966920494383e03
MattG-bci <mateusz.f.grzybowski@gmail.com>
2023-03-25 14:47:00 +0000

    Cleaning the cv branch before the merge

--- cv/vision/test_gpu.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-print(torch.cuda.is_available())
-
-
-
-
--- cv/vision/tests/test.py
+++
@@ -1,5 +0,0 @@
-import PepperAPI
-from PepperAPI import Info
-
-PepperAPI.init("cv")
-Info.Send("SimpleMsg", {"value": "Hello world"})
